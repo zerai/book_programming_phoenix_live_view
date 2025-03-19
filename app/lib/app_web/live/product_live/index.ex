3ad21e961f889e57e691853de3b86a42d0847fe5
zero <teclaizerai@gmail.com>
2025-03-19 13:30:46 +0100

    Added autogenerated code for context 'Catalog'
    - run mix phx.gen.live Catalog Product products name:string \
        description:string unit_price:float sku:integer:unique

---
+++ app/lib/app_web/live/product_live/index.ex
@@ -0,0 +1,47 @@
+defmodule AppWeb.ProductLive.Index do
+  use AppWeb, :live_view
+
+  alias App.Catalog
+  alias App.Catalog.Product
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, stream(socket, :products, Catalog.list_products())}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    socket
+    |> assign(:page_title, "Edit Product")
+    |> assign(:product, Catalog.get_product!(id))
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket
+    |> assign(:page_title, "New Product")
+    |> assign(:product, %Product{})
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Listing Products")
+    |> assign(:product, nil)
+  end
+
+  @impl true
+  def handle_info({AppWeb.ProductLive.FormComponent, {:saved, product}}, socket) do
+    {:noreply, stream_insert(socket, :products, product)}
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    product = Catalog.get_product!(id)
+    {:ok, _} = Catalog.delete_product(product)
+
+    {:noreply, stream_delete(socket, :products, product)}
+  end
+end
